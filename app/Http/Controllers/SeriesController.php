29977644a14bd26b6ed554026a64922ad807a38a
Edmarques Lima <edmarquessantosdelima@gmail.com>
2022-04-07 19:46:06 -0300

    feature: initial commit project

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SeriesController extends Controller
+{
+    public function index(Request $request)
+    {
+        $series = [
+            'Grey\'s Anatomy',
+            'Lost',
+            'Agents of SHIELD'
+        ];
+
+        return view(
+            'series.index',
+            compact('series')
+        );
+    }
+}
